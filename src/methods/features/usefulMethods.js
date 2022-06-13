f119d041baaa6eec2fe96704f81b87a1174889b6
Cuozzo, Giovanni <giovanni.cuozzo@nttdata.com>
2022-06-13 10:37:15 +0200

    +createSVGElem

---
+++ src/methods/features/usefulMethods.js
@@ -0,0 +1,17 @@
+import * as d3 from "d3";
+export const createSVGElem = (elementId, width, height, margin) => {
+  $("#" + elementId).empty();
+  var extSvg = d3
+    .select("#" + elementId)
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+  var svg = extSvg
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("class", "BCT")
+    .attr("id", "BCT" + elementId);
+
+  return svg;
+};
