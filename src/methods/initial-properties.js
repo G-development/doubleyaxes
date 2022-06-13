9dd3127bec1a2e70fbec0e0d3bfd87c5fccab4dc
Cuozzo, Giovanni <giovanni.cuozzo@nttdata.com>
2022-06-13 10:23:34 +0200

    Edit initial-props

--- src/methods/initial-properties.js
+++ src/methods/initial-properties.js
@@ -1,17 +1,12 @@
 export default {
-	qHyperCubeDef: {
-		qDimensions: [],
-		qMeasures: [],
-		qInitialDataFetch: [],
-	},
-	field: {
-		qListObjectDef: {
-			qShowAlternatives: true,
-			qInitialDataFetch: [{
-				qWidth:1,
-				qHeight: 10000
-			}],
-			qSortByState: 0,
-		},
-	},
-}
+  qHyperCubeDef: {
+    qDimensions: [],
+    qMeasures: [],
+    qInitialDataFetch: [
+      {
+        qWidth: 5,
+        qHeight: 2000,
+      },
+    ],
+  },
+};
