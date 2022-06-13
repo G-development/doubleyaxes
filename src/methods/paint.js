1602e1feb9153391a0b2a370a3b49be2b3c378d6
Cuozzo, Giovanni <giovanni.cuozzo@nttdata.com>
2022-06-13 10:25:11 +0200

    Update paint

--- src/methods/paint.js
+++ src/methods/paint.js
@@ -1,7 +1,10 @@
-import './style.css'
-var qlik = window.require('qlik')
+import * as d3 from "d3";
 
+import "./style.css";
+
+var qlik = window.require("qlik");
 
 export default function paint($element, layout) {
-
+  console.log("Layout", layout);
+  
 }
