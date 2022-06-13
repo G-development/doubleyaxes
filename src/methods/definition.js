f70352bc08ab3392bfdd2f53c63b5db6232bd5ed
Cuozzo, Giovanni <giovanni.cuozzo@nttdata.com>
2022-06-13 10:22:41 +0200

    Add dimension, measures

--- src/methods/definition.js
+++ src/methods/definition.js
@@ -1,22 +1,33 @@
-import {cpAbout,cpString,cpDropDownNumber,cpText} from './util'
+import { cpAbout, cpString, cpDropDownNumber, cpText } from "./util";
 
 export default {
-	type: "items",
-	component: "accordion",
-	items: {
-		settings: {
-			uses: "settings",
-		
-		},
-		config: {
-			type: "items",
-			label: "Configuration",
-			items:{
+  type: "items",
+  component: "accordion",
+  items: {
+    dimensions: {
+      uses: "dimensions",
+      min: 1,
+      max: 1,
+    },
+    measures: {
+      uses: "measures",
+      min: 1,
+      max: 2,
+      items: {},
+    },
+    settings: {
+      uses: "settings",
+    },
+    config: {
+      type: "items",
+      label: "Configuration",
+      items: {
+        // Insert component
 				// Insert component 
-			}
-		
-		},
-		
-		about: cpAbout("extension", "1.0.0"),
-	},
-}
+        // Insert component
+      },
+    },
+
+    about: cpAbout("extension", "1.0.0"),
+  },
+};
