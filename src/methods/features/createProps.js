12e594882efa9c483c13286e4beffa5090cb7a51
Cuozzo, Giovanni <giovanni.cuozzo@nttdata.com>
2022-06-14 14:36:54 +0200

    Manage props

--- src/methods/features/createProps.js
+++ src/methods/features/createProps.js
@@ -1,5 +1,20 @@
 export const createProps = (layout) => {
   var dya = layout.DYA;
-  var allProps = {};
+  var allProps = {
+    // Padding
+    paddingGroup:
+      dya.paddingGroup && dya.paddingGroup != null && dya.paddingGroup != ""
+        ? parseInt(dya.paddingGroup)
+        : 0.2,
+    paddingBars:
+      dya.paddingBars && dya.paddingBars != null && dya.paddingBars != ""
+        ? parseInt(dya.paddingBars)
+        : 0.05,
+    // Legend
+    legendSwitch:
+      dya.legendSwitch && dya.legendSwitch != null && dya.legendSwitch != ""
+        ? dya.legendSwitch
+        : false,
+  };
   return allProps;
 };
